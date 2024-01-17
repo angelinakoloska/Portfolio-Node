62870b367fec0bb6bf84864cec0cdde61f7617e3
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-01-17 11:01:50 +0100

    Add validation to POST handler for recommendations

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const fs = require('fs');
 const path = require('path');
 var bodyParser = require('body-parser');
+const { param } = require('jquery');
 var jsonParser = bodyParser.json();
 
 /* GET home page. */
@@ -12,6 +13,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', jsonParser, function(req, res, next) {
+  const expectedAttr = ['avatar', 'name', 'role', 'description'];
+  Object.keys(req.body).forEach(param => {
+    if (!(expectedAttr.includes(param))) {
+      console.log(param);
+      res.status(400).end('Wrong Attr');
+    } else {
+      if(req.body[param] == '') {
+        res.status(400).end(param + ' must have a value');
+      }
+    }
+  });
+  if (req.body.avatar == null || req.body.name == null) {
+    res.status(400).end("Avatar/name not provided");
+  }
+
+    if (![1, 2, 3].includes(req.body.avatar)) {
+      res.status(400).end("Wrong avatar provided");
+    }
+
   let rawdata = fs.readFileSync(path.resolve(__dirname, '../data/recommendations.json'));
   let recommendationsArray = JSON.parse(rawdata);
   if (recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
