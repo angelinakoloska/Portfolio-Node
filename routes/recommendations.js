eb04a38a4160eeef9b234ba96cfcb22c822caf25
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-01-18 13:56:26 +0100

    Add security endpoint for recommendations

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,52 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const fs = require('fs');
-const path = require('path');
-var bodyParser = require('body-parser');
-const { param } = require('jquery');
+const fs = require("fs");
+const path = require("path");
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
+var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
+var ensureLoggedIn = ensureLogIn();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, '../data/recommendations.json'));
-  res.render('recommendations', { data: JSON.parse(data)});
+router.get("/", function (req, res, next) {
+  let data = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
+  res.render("recommendations", { data: JSON.parse(data) });
 });
 
-router.post('/', jsonParser, function(req, res, next) {
-  const expectedAttr = ['avatar', 'name', 'role', 'description'];
-  Object.keys(req.body).forEach(param => {
-    if (!(expectedAttr.includes(param))) {
-      console.log(param);
-      res.status(400).end('Wrong Attr');
-    } else {
-      if(req.body[param] == '') {
-        res.status(400).end(param + ' must have a value');
-      }
-    }
-  });
-  if (req.body.avatar == null || req.body.name == null) {
-    res.status(400).end("Avatar/name not provided");
-  }
-
-    if (![1, 2, 3].includes(req.body.avatar)) {
-      res.status(400).end("Wrong avatar provided");
-    }
-
-  let rawdata = fs.readFileSync(path.resolve(__dirname, '../data/recommendations.json'));
+router.post("/", jsonParser, function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
   let recommendationsArray = JSON.parse(rawdata);
-  if (recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
-    const newArray = recommendationsArray.concat([req.body])
-    fs.writeFileSync(path.resolve(__dirname, '../data/recommendations.json'), JSON.stringify(newArray));
+  if (
+    recommendationsArray.filter((x) => x.name === req.body.name).length == 0
+  ) {
+    const newArray = recommendationsArray.concat([req.body]);
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/recommendations.json"),
+      JSON.stringify(newArray)
+    );
   }
   res.end();
-})
+});
 
-router.delete('/', jsonParser, function(req, res, next) {
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+router.delete("/", jsonParser, ensureLoggedIn, function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
   let recommendationsArray = JSON.parse(rawdata);
-  const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
-  if (newArray.length !== recommendationsArray.length ) {
-    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  const newArray = recommendationsArray.filter((x) => x.name !== req.body.name);
+  if (newArray.length !== recommendationsArray.length) {
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/recommendations.json"),
+      JSON.stringify(newArray)
+    );
   }
   res.end();
 });
